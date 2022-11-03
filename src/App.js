183eb67e87218828d6be8e6de04e910c900e685b
RamS24 <AlSaadi.Rami24@gmail.com>
2022-11-03 21:45:53 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import InputForm from "./InputForm"
 
 function App() {
   return (
     <div className="App">
-        
+        <InputForm label='First Name' />
+        <InputForm label='Last Name' />
+        <InputForm label='Email' type='email'/>
     </div>
   );
 }
